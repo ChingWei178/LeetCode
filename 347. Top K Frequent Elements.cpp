8dbac9c836f2fe07a5332b54f2eac7e30c505828
Ching Wei Su <chingwei.ee10@nycu.edu.tw>
2022-05-18 10:07:27 +0800

    upload prob347

---
+++ 347. Top K Frequent Elements.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int,int> map;
+        for(int num : nums){
+            map[num]++;
+        }
+        
+        vector<int> res;
+        // pair<first, second>: first is frequency,  second is number
+        priority_queue<pair<int,int>> pq; 
+        for(auto it = map.begin(); it != map.end(); it++){
+            pq.push(make_pair(it->second, it->first));
+            if(pq.size() > (int)map.size() - k){
+                res.push_back(pq.top().second);
+                pq.pop();
+            }
+        }
+        return res;
+    }
+};
+
+// time complexity = 
+//                     push into max heap => O(lg(n-k))  heap size = n-k
+//                 x)  n
+//                  =  O(nlg(n-k))
+
+// space complexity = O(n)
