f88f54395ec4576fed05ab2ec60f994db1531610
Ching Wei Su <chingwei.ee10@nycu.edu.tw>
2022-05-18 20:03:06 +0800

    upload prob122 & update prob121

--- 121. Best Time to Buy and Sell Stock.cpp
+++ 121. Best Time to Buy and Sell Stock.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
+    int maxProfit(const vector<int>& prices) {
         int min_price = prices[0];
         int max_profit = 0;
         for (int i = 1; i < prices.size(); i++) {
