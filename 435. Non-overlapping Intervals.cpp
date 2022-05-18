5b18d9cf16901d3a3b357ae365df9f085a4e2401
Ching Wei Su <chingwei.ee10@nycu.edu.tw>
2022-05-19 00:23:18 +0800

    upload prob435

---
+++ 435. Non-overlapping Intervals.cpp
@@ -0,0 +1,24 @@
+bool cmp(const vector<int>&a, const vector<int>&b) {
+        return a[1] < b[1];
+    }
+
+class Solution {
+public:
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end(), cmp);
+        int finish_time = intervals[0][1];
+        int count = 0;
+        for (auto iter = intervals.begin() + 1; iter != intervals.end(); iter++) {
+            if ((*iter)[0] < finish_time) {
+                count++;
+                continue;
+            }
+            else 
+                finish_time = (*iter)[1];
+        }
+        
+        return count;
+    }
+};
+
+// time complexity bound at sorting => O(nlgn), space complexity = O(1)
